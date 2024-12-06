43c87aeacb2255ab06b82b110c654be290e76c9f
Tomas Karas <tomas.karas@fs.cvut.cz>
2024-12-06 16:08:47 +0100

    Added merge sort

--- sorting_algorithms/sorting.py
+++ sorting_algorithms/sorting.py
@@ -61,3 +61,56 @@ def bubble_sort(data):
             break
 
     return array
+
+
+def merge_sort(data):
+
+    # check if input data have more than one element
+    size = len(data)
+    if size > 1:
+
+        # splitting part
+        ############################
+
+        # copy the input data
+        array = data.copy()
+
+        # find middle pint and divide the array into two halves
+        mid = size // 2
+        left_half = array[:mid]
+        right_half = array[mid:]
+
+        # recursively sort the two halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # merging part
+        ############################
+
+        # set initial index for left, right and merged arrays
+        i = j = k = 0
+
+        # start merging until one of the halves is empty
+        while i < len(left_half) and j < len(right_half):
+
+            # compare two elements and place the smaller one in the merged array
+            # go through the halves
+            if left_half[i] < right_half[j]:
+                array[k] = left_half[i]
+                i += 1
+            else:
+                array[k] = right_half[j]
+                j += 1
+
+        # one of the halves is empty, so place the remaining elements in the merged array
+        while i < len(left_half):
+            array[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            array[k] = right_half[j]
+            j += 1
+            k += 1
+
+        return array
